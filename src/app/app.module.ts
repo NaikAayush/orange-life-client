67ea79badb9086bd0d9a5b7be222be3a1d6f0bd7
NaikAayush <ayush@pesu.pes.edu>
2021-07-28 23:56:16 +0530

    create welcome decision page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicNavbarComponent } from './components/navbar/public-navbar/public-navbar.component';
 import { PrivateNavbarComponent } from './components/navbar/private-navbar/private-navbar.component';
+import { WelcomeComponent } from './components/auth/welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, PublicNavbarComponent, PrivateNavbarComponent],
+  declarations: [AppComponent, PublicNavbarComponent, PrivateNavbarComponent, WelcomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
