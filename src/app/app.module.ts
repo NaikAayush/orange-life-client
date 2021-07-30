fba2794d66dc33c5fb744ffac939ccd5378e4ce4
NaikAayush <ayush@pesu.pes.edu>
2021-07-30 10:17:59 +0530

    enable http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
 import { PublicNavbarComponent } from './components/navbar/public-navbar/public-navbar.component';
 import { PrivateNavbarComponent } from './components/navbar/private-navbar/private-navbar.component';
 import { WelcomeComponent } from './components/auth/welcome/welcome.component';
@@ -13,6 +16,7 @@ import { UploadComponent } from './components/dashboard/upload/upload.component'
 @NgModule({
   declarations: [
     AppComponent,
+    HttpClientModule,
     PublicNavbarComponent,
     PrivateNavbarComponent,
     WelcomeComponent,
