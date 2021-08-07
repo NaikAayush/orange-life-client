61ceb5e7c64852c2e710313a85c4875fd88f1f33
NaikAayush <ayush@pesu.pes.edu>
2021-08-07 11:32:06 +0530

    fixed upload component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
   {
     path: 'upload',
     component: UploadComponent,
+    data: { title: 'Upload' },
   },
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Web3Service } from './services/web3/web3.service';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
   mode: string;
   backdrop: string;
   menuState: boolean = false;
   menuItems = ['Dashboard', 'Medical Records', 'Requests', 'Trusted Contacts'];
-  mobile;
+  mobile: boolean;
   events: string[] = [];
   opened: boolean;
+  data: any;
 
-  constructor(private web3: Web3Service) {
+  constructor(
+    private web3: Web3Service,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) {
     console.log(navigator.userAgent);
     var ua = navigator.userAgent;
 
@@ -39,11 +48,24 @@ export class AppComponent {
     }
   }
 
-  setMenuState() {
-    if (this.menuState == true) {
-      this.menuState = false;
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        var rt = this.getChild(this.activatedRoute);
+
+        rt.data.subscribe((data) => {
+          console.log(data);
+          this.data = data;
+          this.titleService.setTitle(data.title);
+        });
+      });
+  }
+  getChild(activatedRoute: ActivatedRoute) {
+    if (activatedRoute.firstChild) {
+      return this.getChild(activatedRoute.firstChild);
     } else {
-      this.menuState = true;
+      return activatedRoute;
     }
   }
 }
